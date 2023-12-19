5f3ce2f5c8cee26bda836093a4becc18d18d5f31
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-20 02:20:34 +0900

    update lozic

--- crypto_env.py
+++ crypto_env.py
@@ -1,24 +1,54 @@
-from enum import Enum
 import gym
 import pandas as pd
 import numpy as np
 from gym import spaces
 from stable_baselines3 import PPO
-import math
+import torch
+from torch.utils.data import Dataset, DataLoader
+from enum import Enum
+from tqdm import tqdm
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-# 데이터 로딩 및 전처리 함수
-def load_and_preprocess_data(filepath):
-    df = pd.read_csv(filepath)
-    df = df[["price", "qty", "time", "isBuyerMaker"]]
-    df["price"] = df["price"].astype(float)
-    df["qty"] = df["qty"].astype(float)
-    df["time"] = pd.to_datetime(df["time"], unit="ms")
-    df["isBuyerMaker"] = df["isBuyerMaker"].astype(int)
-    return df
+class CryptoDataset(Dataset):
+    def __init__(self, csv_file):
+        self.data_frame = pd.read_csv(csv_file)
+        self.data_frame = self.data_frame[["price", "qty", "time", "isBuyerMaker"]]
+        self.extract_time_features()
+        self.normalize()
+
+    def extract_time_features(self):
+        self.data_frame["time"] = pd.to_datetime(self.data_frame["time"], unit="ms")
+        self.data_frame["year"] = self.data_frame["time"].dt.year
+        self.data_frame["month"] = self.data_frame["time"].dt.month
+        self.data_frame["day"] = self.data_frame["time"].dt.day
+        self.data_frame["hour"] = self.data_frame["time"].dt.hour
+        self.data_frame["minute"] = self.data_frame["time"].dt.minute
+        self.data_frame["second"] = self.data_frame["time"].dt.second
+        self.data_frame["millisecond"] = self.data_frame["time"].dt.microsecond // 1000
+        self.data_frame.drop(columns=["time"], inplace=True)
+
+    def normalize(self):
+        numeric_cols = [
+            "price",
+            "qty",
+            "year",
+            "month",
+            "day",
+            "hour",
+            "minute",
+            "second",
+            "millisecond",
+        ]
+        for col in numeric_cols:
+            self.data_frame[col] = self.data_frame[col].astype(float)
+            self.data_frame[col] = (
+                self.data_frame[col] - self.data_frame[col].min()
+            ) / (self.data_frame[col].max() - self.data_frame[col].min())
+        self.data_frame["isBuyerMaker"] = self.data_frame["isBuyerMaker"].astype(int)
 
 
-# 거래 위치 및 행동 정의
 class Positions(int, Enum):
     SHORT = -1
     FLAT = 0
@@ -33,23 +63,26 @@ class Actions(int, Enum):
     DOUBLE_BUY = 4
 
 
-# 암호화폐 거래를 위한 Gym 환경 클래스
 class CryptoTradingEnv(gym.Env):
-    def __init__(self, df, window_size):
+    def __init__(self, dataset, window_size):
         super(CryptoTradingEnv, self).__init__()
-        self.df = df
+        self.dataset = dataset
         self.window_size = window_size
         self.current_step = 0
         self.position = Positions.FLAT
         self.cost = 1  # 기본 비용
 
-        # 행동 공간 정의
         self.action_space = spaces.Discrete(len(Actions))
-
-        # 관찰 공간 정의
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(self.window_size, len(df.columns)), dtype=np.float32
+            low=0,
+            high=1,
+            shape=(self.window_size, len(self.dataset[0])),
+            dtype=np.float32,
         )
+
+        self.trade_fee_bid_percent = 0.005  # 매수 수수료
+        self.trade_fee_ask_percent = 0.005  # 매도 수수료
+        self.raw_prices = self.dataset.data_frame["price"].to_numpy()
 
     def reset(self):
         self.current_step = 0
@@ -57,19 +90,19 @@ class CryptoTradingEnv(gym.Env):
         return self._next_observation()
 
     def _next_observation(self):
-        return self.df.iloc[
+        return self.dataset.iloc[
             self.current_step : self.current_step + self.window_size
         ].values
 
     def step(self, action):
-        prev_price = self.df.loc[self.current_step, "price"]
+        prev_price = self.dataset.loc[self.current_step, "price"]
         self.current_step += 1
-        next_price = self.df.loc[self.current_step, "price"]
+        next_price = self.dataset.loc[self.current_step, "price"]
 
         self.position, trade_made = self._transform(self.position, action)
         reward = self._calculate_reward(prev_price, next_price, trade_made)
 
-        done = self.current_step > len(self.df) - self.window_size
+        done = self.current_step > len(self.dataset) - self.window_size
         obs = self._next_observation()
         return obs, reward, done, {}
 
@@ -102,37 +135,43 @@ class CryptoTradingEnv(gym.Env):
                 position = Positions.LONG
                 trade_made = True
 
-        # Hold 액션의 경우 위치나 상태는 변하지 않습니다.
-        # elif action == Actions.HOLD:
-        #     pass
-
         return position, trade_made
 
-    def _calculate_reward(self, prev_price, next_price, trade_made):
-        # 주어진 로직에 따라 보상 계산
-        if trade_made:
-            if self.position == Positions.LONG:
-                return math.log(next_price / prev_price) + math.log(self.cost)
-            elif self.position == Positions.SHORT:
-                return math.log(2 - next_price / prev_price) + math.log(self.cost)
-        return 0
+    def _calculate_reward(self, action: int) -> np.float32:
+        step_reward = 0.0
+        current_price = self.raw_prices[self.current_step]
+        last_trade_price = self.raw_prices[self.current_step - 1]
+        ratio = current_price / last_trade_price
+        cost = np.log(
+            (1 - self.trade_fee_ask_percent) * (1 - self.trade_fee_bid_percent)
+        )
 
-    def render(self, mode="human"):
-        pass
+        if action == Actions.BUY and self.position == Positions.SHORT:
+            step_reward = np.log(2 - ratio) + cost
 
-    def close(self):
-        pass
+        if action == Actions.SELL and self.position == Positions.LONG:
+            step_reward = np.log(ratio) + cost
+
+        if action == Actions.DOUBLE_SELL and self.position == Positions.LONG:
+            step_reward = np.log(ratio) + cost
+
+        if action == Actions.DOUBLE_BUY and self.position == Positions.SHORT:
+            step_reward = np.log(2 - ratio) + cost
+
+        return float(step_reward)
 
 
-# 데이터 로드
-data = load_and_preprocess_data("prepare_data/output_file.csv")
+# 데이터셋 로드
+crypto_dataset = CryptoDataset("your_data_file.csv")
 
 # 환경 및 에이전트 초기화
-env = CryptoTradingEnv(data, window_size=60)
-model = PPO("MlpPolicy", env, verbose=1)
+env = CryptoTradingEnv(crypto_dataset, window_size=60)
+model = PPO("MlpPolicy", env, verbose=1, device=device)
 
 # 학습
-model.learn(total_timesteps=10000)
+total_epochs = 10
+for _ in tqdm(range(total_epochs), desc="Training Progress"):
+    model.learn(total_timesteps=10000)
 
 # 학습된 모델 저장
 model.save("crypto_trading_ppo")
