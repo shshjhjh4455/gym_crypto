843c2f3b30e154a29647190b95c700189a875fe2
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-20 07:37:45 +0900

    nomalize edit

--- crypto_env.py
+++ crypto_env.py
@@ -9,19 +9,16 @@ from enum import Enum
 from tqdm import tqdm
 import logging
 
-# device = torch.device("mps:0" if torch.backends.mps.is_available() else "cpu")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+
+# device = torch.device("mps:0" if torch.backends.mps.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class CryptoDataset(Dataset):
     def __init__(self, csv_file):
-        # header=None 옵션으로 컬럼명 없이 데이터를 불러온다.
         self.data_frame = pd.read_csv(csv_file, header=None)
-        # 컬럼명을 수동으로 할당한다.
         self.data_frame.columns = [
             "trade_id",
             "price",
@@ -31,37 +28,26 @@ class CryptoDataset(Dataset):
             "isBuyerMaker",
             "isBestMatch",
         ]
-        self.extract_time_features()
+
+        # 필요한 컬럼만 선택
+        self.data_frame = self.data_frame[["price", "qty", "time", "isBuyerMaker"]]
         self.normalize()
 
-    def extract_time_features(self):
-        self.data_frame["time"] = pd.to_datetime(self.data_frame["time"], unit="ms")
-        self.data_frame["year"] = self.data_frame["time"].dt.year
-        self.data_frame["month"] = self.data_frame["time"].dt.month
-        self.data_frame["day"] = self.data_frame["time"].dt.day
-        self.data_frame["hour"] = self.data_frame["time"].dt.hour
-        self.data_frame["minute"] = self.data_frame["time"].dt.minute
-        self.data_frame["second"] = self.data_frame["time"].dt.second
-        self.data_frame["millisecond"] = self.data_frame["time"].dt.microsecond // 1000
-        self.data_frame.drop(columns=["time"], inplace=True)
+    def __getitem__(self, index):
+        return self.data_frame.iloc[index].to_numpy(dtype=np.float32)
+
+    def __len__(self):
+        return len(self.data_frame)
 
     def normalize(self):
-        numeric_cols = [
-            "price",
-            "qty",
-            "year",
-            "month",
-            "day",
-            "hour",
-            "minute",
-            "second",
-            "millisecond",
-        ]
-        for col in numeric_cols:
-            self.data_frame[col] = self.data_frame[col].astype(float)
+        # 'price'와 'qty' 컬럼 정규화
+        for col in ["price", "qty"]:
             self.data_frame[col] = (
                 self.data_frame[col] - self.data_frame[col].min()
             ) / (self.data_frame[col].max() - self.data_frame[col].min())
+
+        # 'time' 컬럼은 정규화 없이 그대로 사용
+        # 'isBuyerMaker' boolean을 int로 변환
         self.data_frame["isBuyerMaker"] = self.data_frame["isBuyerMaker"].astype(int)
 
 
@@ -90,15 +76,12 @@ class CryptoTradingEnv(gym.Env):
 
         self.action_space = spaces.Discrete(len(Actions))
         self.observation_space = spaces.Box(
-            low=0,
-            high=1,
-            shape=(self.window_size, len(self.dataset[0])),
-            dtype=np.float32,
+            low=0, high=1, shape=(self.window_size, len(dataset[0])), dtype=np.float32
         )
 
         self.trade_fee_bid_percent = 0.005  # 매수 수수료
         self.trade_fee_ask_percent = 0.005  # 매도 수수료
-        self.raw_prices = self.dataset.data_frame["price"].to_numpy()
+        self.raw_prices = np.array([item[1] for item in dataset])  # price 데이터
 
     def reset(self):
         self.current_step = 0
@@ -106,19 +89,20 @@ class CryptoTradingEnv(gym.Env):
         return self._next_observation()
 
     def _next_observation(self):
-        return self.dataset.iloc[
-            self.current_step : self.current_step + self.window_size
-        ].values
+        start = self.current_step
+        end = start + self.window_size
+        obs = [self.dataset[i] for i in range(start, min(end, len(self.dataset)))]
+        return np.array(obs)
 
     def step(self, action):
-        prev_price = self.dataset.loc[self.current_step, "price"]
+        prev_price = self.raw_prices[self.current_step]
         self.current_step += 1
-        next_price = self.dataset.loc[self.current_step, "price"]
+        next_price = self.raw_prices[self.current_step]
 
         self.position, trade_made = self._transform(self.position, action)
         reward = self._calculate_reward(prev_price, next_price, trade_made)
 
-        done = self.current_step > len(self.dataset) - self.window_size
+        done = self.current_step >= len(self.dataset) - self.window_size
         obs = self._next_observation()
         return obs, reward, done, {}
 
@@ -153,6 +137,12 @@ class CryptoTradingEnv(gym.Env):
 
         return position, trade_made
 
+    def render(self, mode="human"):
+        pass
+
+    def close(self):
+        pass
+
     def _calculate_reward(self, action: int) -> np.float32:
         step_reward = 0.0
         current_price = self.raw_prices[self.current_step]
@@ -177,25 +167,19 @@ class CryptoTradingEnv(gym.Env):
         return float(step_reward)
 
 
-# 데이터셋 로드 및 DataLoader 설정
-# crypto_dataset = CryptoDataset("prepare_data/output_file.csv")
+# 디버깅 모드 활성화
+torch.autograd.set_detect_anomaly(True)
+
+# 데이터셋 로드 및 환경, 모델 초기화
 crypto_dataset = CryptoDataset("prepare_data/XRPUSDT-trades-2023-11.csv")
-
-data_loader = DataLoader(crypto_dataset, batch_size=1024, shuffle=True)
-
-# 환경 및 에이전트 초기화
 env = CryptoTradingEnv(crypto_dataset, window_size=60)
 model = PPO("MlpPolicy", env, verbose=1, device=device)
 
-# 학습
+# 학습 루프
 total_epochs = 10
-logger.info("Training starts...")
 for epoch in tqdm(range(total_epochs), desc="Training Progress"):
-    for batch in data_loader:
-        env.load_batch(batch)
-        model.learn(total_timesteps=10000)
-    logger.info(f"Epoch {epoch + 1}/{total_epochs} completed.")
+    logging.info(f"Epoch {epoch + 1}/{total_epochs}")
+    model.learn(total_timesteps=10000)
 
 # 학습된 모델 저장
 model.save("crypto_trading_ppo")
-logger.info("Training completed and model saved.")
