8c71a1dfaa9db6ee23c8ec124f6caffd4b7e9af7
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-21 06:20:02 +0900

    오류 해결

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 from gym import spaces
 from stable_baselines3 import PPO
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torch import nn
 from enum import Enum
 from tqdm import tqdm
@@ -17,7 +17,6 @@ logging.basicConfig(level=logging.INFO)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# 데이터셋 클래스 정의
 class CryptoDataset(Dataset):
     def __init__(self, csv_file):
         self.data_frame = pd.read_csv(csv_file, header=None)
@@ -34,7 +33,7 @@ class CryptoDataset(Dataset):
         self.normalize()
 
     def __getitem__(self, index):
-        return self.data_frame[index]
+        return self.data_frame.iloc[index].to_numpy(dtype=np.float32)
 
     def __len__(self):
         return len(self.data_frame)
@@ -50,7 +49,6 @@ class CryptoDataset(Dataset):
         self.data_frame = self.data_frame.to_numpy(dtype=np.float32)
 
 
-# 환경 클래스 정의
 class Positions(int, Enum):
     SHORT = -1
     FLAT = 0
@@ -110,10 +108,12 @@ class CryptoTradingEnv(gym.Env):
 
     def _transform(self, position, action):
         trade_made = False
+
         if action == Actions.DOUBLE_SELL:
             if position in [Positions.FLAT, Positions.LONG]:
                 position = Positions.SHORT
                 trade_made = True
+
         elif action == Actions.SELL:
             if position == Positions.LONG:
                 position = Positions.FLAT
@@ -121,6 +121,7 @@ class CryptoTradingEnv(gym.Env):
             elif position == Positions.FLAT:
                 position = Positions.SHORT
                 trade_made = True
+
         elif action == Actions.BUY:
             if position == Positions.SHORT:
                 position = Positions.FLAT
@@ -128,40 +129,42 @@ class CryptoTradingEnv(gym.Env):
             elif position == Positions.FLAT:
                 position = Positions.LONG
                 trade_made = True
+
         elif action == Actions.DOUBLE_BUY:
             if position in [Positions.FLAT, Positions.SHORT]:
                 position = Positions.LONG
                 trade_made = True
+
         return position, trade_made
 
     def _calculate_reward(self, prev_price, next_price, trade_made):
         step_reward = 0.0
+
         if trade_made:
             ratio = next_price / prev_price
             cost = np.log(
                 (1 - self.trade_fee_ask_percent) * (1 - self.trade_fee_bid_percent)
             )
+
+            # 롱 또는 숏 포지션에 따른 보상 계산
             if self.position == Positions.LONG:
                 step_reward = max(0, np.log(ratio)) + cost
             elif self.position == Positions.SHORT:
                 step_reward = max(0, np.log(2 - ratio)) + cost
+
+            # isBuyerMaker 필드를 활용하여 메이커 거래에 추가 보상 제공
             is_buyer_maker = self.dataset[self.current_step - 1, 3]
-            if is_buyer_maker == 1:
-                additional_reward = 0.01
-                step_reward += additional_reward
+            if is_buyer_maker == 1:  # 메이커 거래인 경우
+                additional_reward = 0.01  # 예시 값, 상황에 맞게 조정 가능
+                step_reward += additional_reward  # 추가 보상 값
+
         return step_reward
 
-    def render(self, mode="human"):
-        pass
 
-    def close(self):
-        pass
-
-
-# 정책 네트워크 클래스 정의
 class CustomActorCriticPolicy(nn.Module):
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super(CustomActorCriticPolicy, self).__init__()
+        # 신경망 레이어 정의
         self.actor = nn.Sequential(
             nn.Linear(4 * 60, 256),
             nn.ReLU(),
@@ -178,13 +181,14 @@ class CustomActorCriticPolicy(nn.Module):
         )
 
     def forward(self, x):
-        x = x.view(x.size(0), -1)
+        # 입력 x를 처리
+        x = x.view(x.size(0), -1)  # Flatten
         return self.actor(x), self.critic(x)
 
 
 # 데이터셋 로드 및 환경 초기화
 crypto_dataset = CryptoDataset("prepare_data/XRPUSDT-trades-2023-11.csv")
-env = CryptoTradingEnv(crypto_dataset, window_size=5000)
+env = CryptoTradingEnv(crypto_dataset, window_size=60)
 
 # PPO 모델 초기화 및 정책 네트워크 설정
 model = PPO(CustomActorCriticPolicy, env, verbose=1, device=device)
