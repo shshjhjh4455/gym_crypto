e4384c470fe0a6a62b76e182b63612ed0d0e2f1d
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-26 07:25:45 +0900

    issue with run_inference

--- crypto_inference.py
+++ crypto_inference.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 from stable_baselines3 import PPO
 import matplotlib.pyplot as plt
-from crypto_env import CryptoTradingEnv  # DataPreprocessor는 사용하지 않음
+from crypto_env import CryptoTradingEnv
+from tqdm import tqdm
 
 
 class InferenceEnvironment:
@@ -19,13 +20,23 @@ class InferenceEnvironment:
         rewards = []
         price_changes = []
 
-        while not done:
+        for _ in tqdm(range(len(self.env.data_frame)), desc="Running Inference"):
+            if done:
+                break
+
             action, _ = self.model.predict(state, deterministic=True)
-            state, reward, done, info = self.env.step(action)
+            next_state, reward, done, info = self.env.step(action)
+
+            if done:
+                break
+
+            price_change_origin = next_state[1]  # price_change_origin에 해당하는 인덱스
             total_rewards += reward
             actions.append(action)
             rewards.append(reward)
-            price_changes.append(state[1])  # price_change_origin에 해당하는 인덱스
+            price_changes.append(price_change_origin)
+
+            state = next_state
 
         self.plot_results(actions, rewards, price_changes)
         print(f"Total rewards obtained: {total_rewards}")
